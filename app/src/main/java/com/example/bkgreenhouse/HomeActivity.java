556ff32ab963e19755b03ad70e92ee044d8f3878
Nhatle <nhatle04072001@gmail.com>
2022-03-15 19:23:24 +0700

    Test on off button

--- app/src/main/java/com/example/bkgreenhouse/HomeActivity.java
+++ app/src/main/java/com/example/bkgreenhouse/HomeActivity.java
@@ -1,29 +1,29 @@
 package com.example.bkgreenhouse;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.ArrayMap;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.bkgreenhouse.databinding.ActivityHomeBinding;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
-import okhttp3.Call;
-import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class HomeActivity extends AppCompatActivity {
     ActivityHomeBinding binding;
     String KEY = "aio_sVox31ArO0laWWiQ0rD8V1cSl5x3";
-    OkHttpClient client;
-    String URL = "https://io.adafruit.com/api/v2/luongcao2202/feeds/bbc-led/data/retain";
+    String LED_GET_URL = "https://io.adafruit.com/api/v2/luongcao2202/feeds/bbc-led/data/retain";
+    String LED_POST_URL = "https://io.adafruit.com/api/v2/luongcao2202/feeds/bbc-led/data";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,39 +31,89 @@ public class HomeActivity extends AppCompatActivity {
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        client = new OkHttpClient.Builder().build();
+        new GetBtn().execute(LED_GET_URL);
 
         binding.btnPush.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Request request = new Request.Builder()
-                        .url(URL)
-                        .build();
-
-                Call call = client.newCall(request);
-                call.enqueue(new Callback() {
-                    @Override
-                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                        Log.d("HomeAc", "Fail");
-                    }
-
-                    @Override
-                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                        Log.d("HomeAc", "Ok");
-                        Log.d("HomeAc", response.body().string());
-                    }
-                });
-
+                toggleBtn();
             }
         });
     }
 
-    private void fail() {
-        Toast.makeText(HomeActivity.this, "ERROR", Toast.LENGTH_SHORT).show();
+    private void toggleBtn() {
+        if (binding.btnPush.getText() == "On") {
+            new PostBtn("0").execute(LED_POST_URL);
+        }
+        else {
+            new PostBtn("1").execute(LED_POST_URL);
+        }
     }
 
-    private void success() {
-        Toast.makeText(HomeActivity.this, "SUCCESS!", Toast.LENGTH_SHORT).show();
+    private void setBtn(String substring) {
+        if (substring.equals("1"))
+            binding.btnPush.setText("On");
+        else
+            binding.btnPush.setText("Off");
     }
 
+    class GetBtn extends AsyncTask<String, String, String>{
+        OkHttpClient client = new OkHttpClient.Builder().retryOnConnectionFailure(true).connectTimeout(15, TimeUnit.SECONDS)
+                .build();
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Request request = new Request.Builder()
+                    .url(strings[0])
+                    .build();
+
+            try {
+                Response response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            setBtn(s.substring(0,1));
+            super.onPostExecute(s);
+        }
+    }
+
+    class PostBtn extends AsyncTask<String, String, Void>{
+        OkHttpClient client = new OkHttpClient.Builder().retryOnConnectionFailure(true).connectTimeout(15, TimeUnit.SECONDS)
+                .build();
+        String value;
+        PostBtn(String value){
+            this.value = value;
+        }
+        @Override
+        protected Void doInBackground(String... strings) {
+            String json = "{\"datum\":{\"value\":\""+ value +"\"}}";
+
+            RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), json);
+            Request request = new Request.Builder()
+                    .url(strings[0])
+                    .addHeader("X-AIO-Key","aio_Juvs623hQrLI7NCNgarHhflzP8Od")
+                    .addHeader("Content-Type", "application/json")
+                    .post(requestBody)
+                    .build();
+
+            try {
+                Response response = client.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            setBtn(value);
+            super.onPostExecute(unused);
+        }
+    }
 }
